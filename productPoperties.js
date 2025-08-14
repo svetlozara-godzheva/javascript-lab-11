e9bf96b747744880b6b3c08f001269d349ed4435
svetlozara-godzheva <n01754024@humber.ca>
2025-08-14 16:47:50 -0400

    Added a method getTotalValue

--- productPoperties.js
+++ productPoperties.js
@@ -1,7 +1,11 @@
 class ProductProperties {
-        constructor(name, price, quantity) {
+    constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+    getTotalValue() {
+        let totalValue = this.price * this.quantity;
+        return totalValue;
     }
 }
