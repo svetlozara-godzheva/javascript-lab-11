12f61fbe0589c34e7ae2193d5cb57becee21ac94
svetlozara-godzheva <n01754024@humber.ca>
2025-08-14 17:27:20 -0400

    Instantiated the subclass

--- perishableProductProperties.js
+++ perishableProductProperties.js
@@ -9,3 +9,8 @@ class PerishableProductProperties extends ProductProperties {
         return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
+
+let eggs = new PerishableProductProperties("eggs", 3.93, 12, "2025-08-28");
+let mayonnaise = new PerishableProductProperties("Hellmann's Real Mayonnaise", 6.50, 1, "2025-12-14");
+console.log(eggs.toString());
+console.log(mayonnaise.toString());
